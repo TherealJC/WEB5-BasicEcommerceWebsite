a41b6f0cd0587292ce7cd0f82f38ca00b90dcc72
unknown <Jamie-crowther@live.nmit.ac.nz>
2020-11-23 18:41:33 +1300

    Complete

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-//function signUpUpser() It has been set to run when the user clicks the submit button on the sign up page.
-function signUpUser(){
-    var username = document.getElementById('username') //set variables equal to the input fields
+//function registerUpser() has been set to run when the user clicks the submit button on the sign up page.
+//It checks that all required fields are valid, and adds the users details to the local storage to use for the log in function
+
+function registerUser(){
+    var username = document.getElementById('username'); //set variables equal to the input fields
     var email = document.getElementById('email');
     var password = document.getElementById('password');
     var password2 = document.getElementById('password2');
@@ -24,41 +26,148 @@ function signUpUser(){
         alert('Please fill in email and password');
 
     } else { //successful account creation, saves the users input to local storage
+        localStorage.setItem('username', username.value);
         localStorage.setItem('email', email.value);
         localStorage.setItem('password', password.value);
         alert('Your account has been successfully created, you will now be redirected to the Log In screen');
-        window.open('/pages/login.html'); //opens the login page after details have been added to localstorage.
+        window.open('/pages/login.html' , '_self'); //opens the login page after details have been added to localstorage.
     } return false; //similar to prevent default, in that it stops the page from submitting when click function is run and input details are incorrect
 }
 
-// Check, checks if the users input matches an existing registry in localstorage
+//----------------------------------------------------------------------------------------------------------
+
+//loginUser() checks if the users input account details match an existing registry in localstorage (declared through sign up page or registerUser function)
 function logInUser(){
-    var savedEmail = localStorage.getItem('email');
-    var savedPassword = localStorage.getItem('password');
+    var savedEmail = localStorage.getItem('email'); //gets the email from localStorage from register function, and applies it to a js variable
+    var savedPassword = localStorage.getItem('password'); //retrieves the password from localStorage and applies to var
 
-    var inputEmail = document.getElementById('inputEmail');
-    var inputPassword = document.getElementById('inputPassword');
+    var inputEmail = document.getElementById('inputEmail'); //gets users input email when function is run
+    var inputPassword = document.getElementById('inputPassword'); //gets users input password when function is run
 
-    if(inputEmail.value == savedEmail && inputPassword.value == savedPassword){
-        alert('Log in succeeded, you will now be redirected to the Shopping page.');
-        window.open('/pages/shop.html');
+    if(inputEmail.value == savedEmail && inputPassword.value == savedPassword){ //if users input matches existing localStorage entry, proceed
+        alert('Log in succeeded, you will now be redirected to the Home page.');
+        window.open('../index.html','_self'); //opens new window (_self makes the page open on the same tab)
     } else {
         alert('Incorrect Username or Password, Please Try Again');
-    } return false; //prevents default, stops the forms from submitting/resetting when onclick function is run
+    } return false; //like prevents default, stops the forms from submitting/resetting when onclick function is run
 }
 
+//----------------------------------------------------------------------------------------------------------
 
-// //Setting a click event function for the submit button, 
-// document.querySelector('#form').addEventListener('click', function(event) {
-//         var savedEmail = localStorage.getItem('email');
-//         var savedPassword = localStorage.getItem('password');
-//         var inputEmail = document.getElementById('inputEmail');
-//         var inputPassword = document.getElementById('inputPassword');
+//Checks the input fields in the payment page
+function checkPayment(){
+    var cardNo = document.getElementById('cardNo');
+    var cvc = document.getElementById('CVC');
+
+    if (cardNo.value.length == 0){ //checks card number length, returns error if empty
+        alert('Please enter your card number');
+    } 
+    else if (cardNo.value.length <16 || cardNo.value.length >=19) { //If cardNo length is LESS THAN 16 or more than 19, return error, this allows for 16 digits (and 3 hyphens).
+        alert('The Card number must be be 16 digits, hyphens can be used in regular spaces');
+    } 
+    else if (cvc.value.length == 0){ //return error if cvc is empty
+        alert('Please enter your cards CVC');
+    } 
+    else if (cvc.value.length == 1 || cvc.value.length == 2 || cvc.value.length >3 ){ //return error if CVC length 1, 2, or more than 3. This only accepts 3 digits max
+        alert('CVC only contains 3 digits');
+    } 
+    else { //If successful, transfer back to shopping page.
+        alert('Transaction Success ! (Nothing happens, you will now be redirected to the Shopping page')
+        window.open('/pages/shop.html','_self');
+    } return false;
+}
+
+//----------------------------------------------------------------------------------------------------------
+
+//Creating a dynamic home page clock
+//set DOM elements
+const time = document.getElementById('time');
+const greeting = document.getElementById('greeting');
+const name = document.getElementById('name');
+
+// Options
+const showAmPm = true; 
+
+function showCurrentTime() {
+    let today = new Date(), //create current date and time
+     hour = today.getHours(), //(number between 0-23) sets var equal to current times hours
+     min = today.getMinutes(),
+     sec = today.getSeconds();
     
-//         if(inputEmail.value == savedEmail && inputPassword.value == savedPassword){
-//             alert('Congratulations, You are now logged in, you will now be redirected to the Home Page.');
-//         }else{
-//             alert('Incorrect Username or Password, Please Try Again');
-//         }
-//     event.preventDefault();
-// }, false);
+    //Set AM or PM
+    const amOrPm = hour >= 12 ? 'PM' : 'AM'; //if current hour is greater than or equal to 12 show PM, else show AM
+
+    //change to a 12 hour format
+    hour = hour % 12 || 12; //stops time format from going over 12
+
+    //Output time, pushes curent time to update the innerHTML of the time tag
+    time.innerHTML = `${hour}<span>:</span>
+    ${addZero(min)}<span>:</span>
+    ${addZero(sec)}
+    ${showAmPm ? amOrPm : ''}`;
+
+    //call function every second to update clock time
+    setTimeout( showCurrentTime, 1000);
+}
+
+//Adds zeroes into the date to format correctly, makes 1:01:01 instead of 1:1:1
+function addZero(n) { //called on minutes and seconds
+    return (parseInt(n, 10) < 10 ? '0' : '') + n; //If 'n' (current minutes/seconds) = less than 10, add 0, else add nothing, add the date number add the end
+}
+
+//----------------------------------------------------------------------------------------------------------
+
+//Change the greeting text depending on time of day
+function setGreeting() {
+    let today = new Date(),
+    hour = today.getHours();
+    
+    if (hour < 12) {
+        // Morning home screen
+        document.body.style.backgroundImage = "url('https://i.ibb.co/7vDLJFb/morning.jpg')";
+        greeting.textContent = "Good Morning, It's a beautiful day. Welcome";
+    
+    } else if (hour < 18) {
+        // Midday home screen
+        document.body.style.backgroundImage = "url('https://i.ibb.co/3mThcXc/afternoon.jpg')";
+        greeting.textContent = 'Good Afternoon, Welcome to The Crowther Express';
+
+    } else {
+        // Night time home screen
+        document.body.style.backgroundImage = "url('https://i.ibb.co/924T2Wv/night.jpg')";
+        greeting.textContent = "Good Evening, Welcome to The Crowther Express";
+        document.body.style.color = 'white';
+      }
+    }
+
+//----------------------------------------------------------------------------------------------------------
+
+// Get Name, changes the NAME displayed on home page depending on Username input when registering.
+function getUsersName() {
+    if (localStorage.getItem('username') === null) {
+      name.textContent = '';
+    } else {
+      name.textContent = localStorage.getItem('username');
+    }
+  }
+
+//----------------------------------------------------------------------------------------------------------
+
+//This function displays the users name in the header of home page if they have registered their acc to local storage
+const userAcc =  document.getElementById('userAcc'); //set var for p tag in header to display username
+
+function displayUser() {
+    if (localStorage.getItem('username') === null) { //If no username has been set, 
+        userAcc.textContent = 'Unregistered User';
+      } else {
+        userAcc.textContent = localStorage.getItem('username'); //change the tags inner html to the users localstorage username
+      }
+}
+
+//----------------------------------------------------------------------------------------------------------
+
+//Call the functions
+showCurrentTime(); //Dynamic clock
+getUsersName(); //display current username in home page welcome
+displayUser(); //Displays cuurent user in header
+setGreeting(); //changes the home page text and background image depending on the clock time
--- readme.md
+++ readme.md
@@ -13,7 +13,50 @@ To keep it clean and start fresh, I removed all previous commits. This was done 
 
 ### Troubleshooting the login/signup forms resetting
 
-It took me quite a bit of trial and error to get the form to stop resetting. All the info I found was with 'click' events and the event functions.
-I found this page which helped me figure out that I can use return false, and to also you must have the 'return' in the html submit tag aswell.
+At first, I wasn't sure if I should hard code in some user account objects, and then write a loop to check all object records in local storage and return success if the users input matches an existing registration. I decided to only accept Log In's that the user has entered into local storage themselves through the registration/signup function.
+When the user completes the sign up form, the registration function is run, it checks that all the fields meet the requirements, and then adds the users input email and password to the browsers localstorage.
+The user is then redirected to the log in screen to enter their newly created account details. This function also checks that all fields meet the requirements, and if a matching email and password are found, the user recieves a success message and is redirected to the shopping page.
 
-Stack Overflow. (August 14, 2011).  How to prevent default event handling in an onclick method. https://stackoverflow.com/questions/7056669/how-to-prevent-default-event-handling-in-an-onclick-method
+It took me quite a bit of trial and error to get the form to stop resetting. All the info I found was with 'click' events and the event functions.
+I found this page -'How to prevent default event handling in an onclick method.' (August 14, 2011). which helped me figure out how that I could use return false at the end of the function, this stops the form from always submitting and resetting when the function is called/button is clicked. For the false method, I found that it would not work if i didnt also have the 'return' in the html onclick tag aswell.
+
+onclick='registerUser'(); would not work, but onclick='return registerUser'(); would work, as it gives it a real return value.
+
+Stack Overflow. (August 14, 2011).  How to prevent default event handling in an onclick method. https://stackoverflow.com/questions/7056669/how-to-prevent-default-event-handling-in-an-onclick-method.
+
+### Payment form function
+
+This function I wrote checks if the input fields are equal to 0, similar to the login and register functions.
+Once successful the user will be redirected back to the home screen where they can see a dynamic greeting and background depending on the time of day.
+
+For the login, register, and payment pages forms and form inputs, I used the SignUp-Javascript repo from Ali for reference on the styling. -Ali Kahwaji. (October 20, 2020). SignUp-JavaScript. GitHub https://github.com/alikahwaji/SignUp-JavaScript.
+
+### Cleaning up code
+
+I then went through and tidied up all of the html and css. I have added in comments to explain most of whats happening on each page/function/style.
+I removed redundant code and improved the existing design and layout of the website.
+
+### Making the home page a dynamic display 
+
+I have made multiple functions to add some dynamic features to the website, primarily on the home/landing page.
+
+1. I have included a clock that updates every second. This is a great feature that I saw while studying. This video was the biggest help to me for this area -Traversy Media, (June 11, 2019), 'Dynamic Landing Page'.
+2. I have wrote a function for the home page that checks if a user has registered, if they have, it retrieves their username from localstorage, and displays it on the header, letting the user know that they are logged into their account.
+3. The home page greeting also changes depending on the users username in localstorage, it will use the users name for the greeting text.
+
+### Considerations
+
+1. In a real scenario, localstorage and JS code would never be used to store sensitive PII, as this is a huge security risk and is very simple to access the data. For this type of site, software like Firebase would be used to securely store and retrieve users account information.
+As we were practicing with the localStorage, i just kept this as is.
+
+2. Some Input entry fields dont check for all requirements, such as email doesnt check if its a valid email.
+The payment form function has been set up to rigorously check that the input data meets the requirements, such as cvc being only 3 digits etc.
+
+
+#### References / Resources Used
+
+Ali Kahwaji. (October 20, 2020). SignUp-JavaScript. GitHub https://github.com/alikahwaji/SignUp-JavaScript.
+
+Stack Overflow. (August 14, 2011).  How to prevent default event handling in an onclick method. https://stackoverflow.com/questions/7056669/how-to-prevent-default-event-handling-in-an-onclick-method.
+
+Traversy Media. (June 11, 2019). Dynamic Landing Page | Javascript. Youtube. https://www.youtube.com/watch?v=fSTQzlprGLI&t=1049s
